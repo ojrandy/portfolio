407f65dc836f4787c84920e54017fc8538426fc5
ojong <ojongrandy01@gmail.com>
2025-12-03 16:58:42 +0100

    Add ShowcaseSection component and integrate into App
    Introduce a new `ShowcaseSection` component with basic structure and styles. The component is imported and rendered in `App.jsx` to expand the application's section layout.

---
+++ src/sections/ShowcaseSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ShowcaseSection = () => {
+    return (
+        <div id="work" className="app-showcase">
+            <div className="w-full">
+                <div className="showcaselayout">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+export default ShowcaseSection
